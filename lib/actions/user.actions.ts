c1b394a76359bf161ee4dbba9d50235a881d6c61
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-12-21 18:52:11 +0100

    add updateUser function in user.actions.ts

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,34 @@
 'use server'
 
+import User from '../models/user.models';
 import { connectToDB } from '../mongoose';
 
-export async function updateUser(): Promise<void> {
-    connectToDB();
+export async function updateUser(
+    userId: string,
+    username: string,
+    name: string,
+    bio: string,
+    image: string,
+    path: string,
+): Promise<void> {
+    connectToDB(); 
+
+try {
+    await User.findOneAndUpdate(
+        { id: userId },
+        {username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onboarded: true, 
+        },
+        {upsert: true},
+    );
+
+    if(path === '/profile/edit') {
+        revalidatePath(path);
+    }
+    } catch (error: any) {
+        throw new Error(`Failed to create/update user: ${error.message}`)
+    }
 }
