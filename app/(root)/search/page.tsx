4025d6c8c205342e0f70d14b60c301c45a86ad63
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-12-30 17:00:54 +0100

    create search page.tsx & add UserCard.tsx

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,42 @@
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import Image from 'next/image';
+import { profileTabs } from '@/constants';
+import ThreadsTab from '@/components/shared/ThreadsTab';
+import UserCard from '@/components/cards/UserCard';
+
+async function Page() {
+    const user = await currentUser();
+    if(!user) return null;
+    const userInfo = await fetchUser(user.id);
+    if(!userInfo?.onboarded) redirect('/onboarding');
+
+    // Fetch users
+    const result = await fetchUsers({
+        userId: user.id,
+        searchString: '',
+        pageNumber: 1,
+        pageSize: 25
+    })
+
+    return (
+        <section>
+            <h1 className="head-text mb-10">Search</h1>
+            <div className='mt-14 flex flex-col gap-9'>
+                {result.users.length === 0 ? (
+                    <p className='no result'>No users</p>
+                ) : (
+                    <>
+                        {result.users.map((person) => (
+                            <UserCard />
+                        ))}
+                    </>
+                )}
+            </div>
+        </section>
+    )
+};
+
+export default Page;
