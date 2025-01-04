f8d6406b32346e11e652635b77bce6b113aacabf
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2025-01-04 17:48:53 +0100

    add communities to comment section at ThreadCard.tsx

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -58,7 +58,15 @@ const ThreadCard = ({ id, currentUserId, parentId, content, author, community, c
                         </div>
                     </div>
                 </div>
+                {/* TODO: DeleteThread */}
+                {/* TODO: Show comment logos */}
             </div>
+            {!isComment && community && (
+                    <Link href={`/communities/${community.id}`} className="mt-5 flex items-center">
+                        <p className="text-subtle-medium text-gray-1">{formatDataString(createdAt)}{' '} - {community.name} Community</p>
+                        <Image src={community.image} alt={community.name} width={14} height={14} className="ml-1 rounded-full object-cover" />
+                    </Link>
+                )}
         </article>
     )
 };
