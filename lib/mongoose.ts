92ad137b1b41e7bed9c56d8d90e20f7b87fe98ee
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2025-01-13 22:02:29 +0100

    add descriptive comments

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -3,14 +3,17 @@ import mongoose from 'mongoose';
 let isConnected = false; //variable to check if mongoose is connected
 
 export const connectToDB = async () => {
+    // Set strict query mode for Mongoose to prevent unknown field queries
     mongoose.set('strictQuery', true);
 
-    if(!process.env.MONGODB_URL) return console.log('MONGODB_URL not foundd')
+    if(!process.env.MONGODB_URL) return console.log('Missing MongoDB URL');
+
+    // If the connection is already established, return without creating a new connection.
     if(isConnected) return console.log('Already connected to MongDB')
 
     try {
         await mongoose.connect(process.env.MONGODB_URL);
-        isConnected = true;
+        isConnected = true; // Set the connection status to true
         console.log('Connected to MongoDB');
     } catch (error) {
         console.log(error);
